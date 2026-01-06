ee49e7a97b68a954a93df6d9a5eae138919742d4
RIneelynn <ajwaassyifa114@gmail.com>
2026-01-06 13:41:30 +0700

    Update script.js

--- script.js
+++ script.js
@@ -35,3 +35,22 @@ function playMusic() {
     }
   }, 100);
 }
+
+// COUNTDOWN
+const targetDate = new Date("2026-12-12T09:00:00").getTime();
+
+setInterval(() => {
+  const now = new Date().getTime();
+  const distance = targetDate - now;
+
+  if (distance < 0) return;
+
+  document.getElementById("days").innerText =
+    Math.floor(distance / (1000 * 60 * 60 * 24));
+  document.getElementById("hours").innerText =
+    Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  document.getElementById("minutes").innerText =
+    Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  document.getElementById("seconds").innerText =
+    Math.floor((distance % (1000 * 60)) / 1000);
+}, 1000);
